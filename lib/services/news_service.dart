9ffbeca50fc06cd0f32f09180c0841af6902602c
Shaahil <shaahilseedin@gmail.com>
2026-02-13 02:49:36 +0530

    fixed a error cause the file got corrupted again

--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -1,11 +1,10 @@
 import 'dart:convert';
 import 'package:flutter/services.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import '../models/article.dart';
 
 class NewsService {
-  final String _apiKey = dotenv.env['NEWS_API_KEY'] ?? '';
+  final String _apiKey = '64db3c3bed724b6289817e01f987217e';
   final String _baseUrl = 'https://newsapi.org/v2';
 
   Future<List<Article>> getTopHeadlines({String? category}) async {
@@ -13,9 +12,9 @@ class NewsService {
       final url = category != null
           ? '$_baseUrl/top-headlines?country=us&category=$category&apiKey=$_apiKey'
           : '$_baseUrl/top-headlines?country=us&apiKey=$_apiKey';
-      
+
       final response = await http.get(Uri.parse(url));
-      
+
       if (response.statusCode == 200) {
         final data = json.decode(response.body);
         final articles = (data['articles'] as List)
@@ -33,9 +32,10 @@ class NewsService {
 
   Future<List<Article>> searchNews(String query) async {
     try {
-      final url = '$_baseUrl/everything?q=$query&apiKey=$_apiKey&sortBy=publishedAt';
+      final url =
+          '$_baseUrl/everything?q=$query&apiKey=$_apiKey&sortBy=publishedAt';
       final response = await http.get(Uri.parse(url));
-      
+
       if (response.statusCode == 200) {
         final data = json.decode(response.body);
         final articles = (data['articles'] as List)
@@ -51,7 +51,8 @@ class NewsService {
   }
 
   Future<List<Article>> _getOfflineNews() async {
-    final jsonString = await rootBundle.loadString('assets/json/offline_news.json');
+    final jsonString =
+        await rootBundle.loadString('assets/json/offline_news.json');
     final List<dynamic> jsonList = json.decode(jsonString);
     return jsonList.map((json) => Article.fromJson(json)).toList();
   }
